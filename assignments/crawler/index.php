af124e4e22ab03ee4de5d9ed73ad4607f6977f1b
Ted Bataller <ted.jerbybataller@gmail.com>
2023-01-26 08:50:10 +0800

    updated php assignments

--- assignments/crawler/index.php
+++ assignments/crawler/index.php
@@ -1,23 +1,21 @@
 <?php
-    //connecting to a remote url using curl
+    include("simple_html_dom.php");
+    $html = file_get_html('https://www.bing.com/search?q=software+engineer&form=QBLH&sp=-1&pq=software+enginer&sc=10&qs=n&sk=&cvid=2F2C43C1EDBC4E6D88C117A7CB766CD5&ghsh=0&ghacc=0&ghpl=');
 
-    //see http://www.php.net/manual/en/function.curl-setopt.php for more info
-    $url = "http://www.espn.com";
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); 
-    $data = curl_exec($ch);
-    $info = curl_getinfo($ch);  
-    curl_close($ch);
+    $titles = array();
 
-    echo "<h1>Data</h1>";
-    echo "<pre>".htmlentities($data)."</pre>";
-
-    echo "<h1>Info</h1>";
-    echo "<pre>";
-    var_dump($info);
-    echo "</pre>";
-
+    foreach($html->find('h2>a') as $t){
+        $titles[] = array("title"=>$t->innerTexT(),"link"=>$t->href);
+    }
+?>
+<?php
+    foreach($titles as $index=>$title){
+        if($index>4){
+            return;
+        }
+?>
+<h1><?= $index+1?>) <?= $title["title"]?></h1>
+<a href="<?=$title["link"]?>"><?=$title["link"]?></a><br>
+<?php
+    }
 ?>
